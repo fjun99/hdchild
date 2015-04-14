c2e61cdb34505ee089bb62aeccefec2b8fba0fc9
fangjun <fjun99@gmail.com>
2015-04-14 16:13:29 +0800

    footer cleaning

--- footer.php
+++ footer.php
@@ -20,17 +20,15 @@
 		?>
 
 		<?php do_action( 'wpbootstrap_before_footer' ); ?>
-		<?php if ( wpbootstrap_get_setting('general_settings', 'display_footer' ) ): ?>
-		    <?php if (of_get_option('display_credit_footer')) : ?>
-			<footer id="footer" class="muted">
-                <hr>
-				<p class="pull-left"><?php echo of_get_option('display_credit_footer_left'); ?></p>
-				<p class="pull-right"><?php echo of_get_option('display_credit_footer_right'); ?></p>
 
-                <p style="clear:both;"><?php echo get_num_queries(); ?> queries in <?php timer_stop(1); ?>  seconds.</p>
-			</footer>
-			<?php endif; ?>
-		<?php endif; ?>
+<footer id="footer" class="muted">
+    <hr>
+    <p class="pull-left"><a href="/">会读：少儿阅读指导</a></p>
+    <p class="pull-right">© 2015</p>
+
+    <p style="clear:both;"><?php echo get_num_queries(); ?> queries in <?php timer_stop(1); ?>  seconds.</p>
+</footer>
+
 		<?php do_action( 'wpbootstrap_after_footer' ); ?>
 
 	</div><!-- .container -->
