3153f659b57f7876d6efc31d84ed7247ffccfb84
fangjun <fjun99@gmail.com>
2015-04-14 16:09:53 +0800

    add single.php( remove navigation)

---
+++ single.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ */
+get_header(); ?>
+
+	<?php
+		while ( have_posts() ) : the_post();
+			get_template_part( 'content', get_post_format() );
+		endwhile;
+	?>
+
+<?php get_footer();
