cb30401094c5c5239f4ef91b29e68f41437761cc
fangjun <fjun99@gmail.com>
2015-04-16 12:08:45 +0800

    test ajax

--- functions.php
+++ functions.php
@@ -88,3 +88,42 @@ remove_action('wp_head', 'feed_links_extra', 3); // Remove category feeds
 remove_action('wp_head', 'feed_links', 2); // Remove Post and Comment Feeds
 
 remove_action('wp_head', 'wp_generator');
+
+
+
+//test
+
+
+
+add_action( 'admin_footer', 'my_action_javascript' ); // Write our JS below here
+
+function my_action_javascript() { ?>
+    <script type="text/javascript" >
+        jQuery(document).ready(function($) {
+
+            var data = {
+                'action': 'my_action',
+                'whatever': 1234
+            };
+
+            // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+            $.post(ajaxurl, data, function(response) {
+                alert('Got this from the server: ' + response);
+            });
+        });
+    </script> <?php
+}
+
+add_action( 'wp_ajax_my_action', 'my_action_callback' );
+
+function my_action_callback() {
+    global $wpdb; // this is how you get access to the database
+
+    $whatever = intval( $_POST['whatever'] );
+
+    $whatever += 10;
+
+    echo $whatever;
+
+    wp_die(); // this is required to terminate immediately and return a proper response
+}
