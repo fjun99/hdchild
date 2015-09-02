40685fb36fa7140e13de4e8b2f6667af1f5fe951
fangjun <fjun99@gmail.com>
2015-09-02 21:49:52 +0800

    nav collapse

--- _navbar.php
+++ _navbar.php
@@ -1,6 +1,12 @@
 <div class="clearfix <?php echo wpbootstrap_get_nav_menu_classes(); ?>">
 	<div class="navbar-inner">
 		<div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+
             <a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
                 <img src="<?php echo get_stylesheet_directory_uri() ?>/huidu_logo.png" class="site-logo">
             </a>
