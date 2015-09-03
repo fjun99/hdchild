2e961941714c6b63f5f11f31a530f352bcff5dbc
fangjun <fjun99@gmail.com>
2015-09-03 21:24:38 +0800

    head cleaning

--- functions.php
+++ functions.php
@@ -89,7 +89,10 @@ remove_action('wp_head', 'feed_links', 2); // Remove Post and Comment Feeds
 
 remove_action('wp_head', 'wp_generator');
 
-
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 //test ajax
 
